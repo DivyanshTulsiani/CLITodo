4e4a6aa4bedf36205f4b64a9387fcf3544dd2170
Divyansh Tulsiani <divyanshtulsiani01@gmail.com>
2025-03-22 00:56:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { Command } = require("commander");
+ const program = new Command();
+
+ program 
+ .name('CLI-Based-Todo')
+ .description('CLI based todo tool to manage your todos on the terminal')
+ .version('1.0.0');
+
+ 
